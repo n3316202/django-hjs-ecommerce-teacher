b2d5b1859b90e3d6c9656e0fbc13447aa6e460e4
heo <admin@admin.com>
2025-03-31 14:23:28 +0900

    commit

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# dev_8
+app_name = "store"
+
 # dev_1
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about", views.about, name="about"),  # dev_8 어바웃 페이지 추가
 ]
