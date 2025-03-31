b9741afabf38710571769ab0cbed36c7f94920e1
heo <admin@admin.com>
2025-03-31 15:21:47 +0900

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+# dev_9
+# User 계정을 커스텀 마이징 시키는 방법은 4가지 정도가 있음
+# 1) proxy 활용
+# 2) AbstractUser 상속 하는 방법
+# 3) AbstractBaseUser 을 상속 하는 방법
+class User(AbstractUser):
+
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
+    gender = models.CharField(
+        verbose_name="성별", max_length=1, choices=GenderChoices.choices
+    )
