2b7b67c41151f38c2d728631a068ff2d5ec6d73c
heo <admin@admin.com>
2025-04-03 12:37:39 +0900

    commit

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 # Create your views here.
+
+
+# dev_15
+def add_cart(request):
+    cart = Cart(request)
+
+    print("카트======", cart)
+
+    if request.POST.get("action") == "post":
+
+        # 상품 받아오기
+        product_id = int(request.POST.get("product_id"))
+        print("product_id", product_id)
