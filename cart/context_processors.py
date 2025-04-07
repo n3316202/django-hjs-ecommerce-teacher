8ae167ee4b8ae3bb9ea20995c020761d3e717a74
heo <admin@admin.com>
2025-04-07 10:50:17 +0900

    commit

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -5,7 +5,7 @@ from .cart import Cart  # 현재폴더 cart.py 에서 Cart객체를 가져옴
 def cart(request):
 
     # 세션확인 테스트
-    # cart.decrypt_all_sessions()
+    #cart.decrypt_all_sessions()
     print("카트 함수 호출")
 
     return {"cart": Cart(request)}
