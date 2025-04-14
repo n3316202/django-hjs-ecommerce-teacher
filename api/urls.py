69199966a84c472f12c8f5d14102da9a87abe808
heo <admin@admin.com>
2025-04-14 14:58:03 +0900

    commit

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+# dev_28
+from api.views import hello_world, hello_world_json, hello_world_drf
+
+app_name = "api"
+urlpatterns = [
+    path("hello-world/", hello_world),
+    path("hello-world-json/", hello_world_json),
+    path("hello-world-drf/", hello_world_drf),
+]
