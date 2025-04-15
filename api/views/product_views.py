aaaac1870def159571775072fd2b8b236cb8c9c9
heo <admin@admin.com>
2025-04-15 15:04:48 +0900

    commit

--- api/views/product_views.py
+++ api/views/product_views.py
@@ -25,10 +25,32 @@ def products_api(request):
     # dev_30
     # 디시리얼라이져
     if request.method == "POST":
-        print(request.data)
+        print("데이터", request.data)  # json , dic
+        print("타입", type(request.data))  # json , dic
+
         serializer = ProductSerializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data)
+
+
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+
+
+# dev_30
+@api_view(["GET", "DELETE"])
+def product_api(request, pk):
+    product = get_object_or_404(Product, id=pk)
+
+    if request.method == "GET":
+        # many=True ➜ 여러 개의 인스턴스 (QuerySet, 리스트 등)
+        # many=False (기본값) ➜ 단일 인스턴스
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        product.delete()
+        return Response("SUCCESS", status=status.HTTP_204_NO_CONTENT)
