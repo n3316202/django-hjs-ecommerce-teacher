9abdc25ba76437abd9200362c3dc43f9788995f0
heo <admin@admin.com>
2025-04-18 11:22:14 +0900

    commit

--- api/urls.py
+++ api/urls.py
@@ -24,9 +24,14 @@ urlpatterns = [
     path("products/", product_views.products_api),
     path("product/<int:pk>/", product_views.product_api),
     # dev_32
-    # 방식   url                  기능
-    # GET  categories/            list
     # path("categories/", category_views.categories_api),
     # dev_35
+    # 방식   url                  기능
+    # GET  categories/           list
+    # POST categories/           create
+    # Get  category/{id}       category
+    # PUT  category/{id}       modify category
+    # DELETE  category/{id}    delete category
     path("categories/", category_views.CategoriesAPI.as_view()),
+    path("category/<int:pk>/", category_views.CategoryAPI.as_view()),
 ]
